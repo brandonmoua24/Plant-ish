ba8d327de7e3849fc89a2ec9a6283bd3243e5b40
Jack-Forthman <j.h.forthman@gmail.com>
2023-11-19 21:17:52 -0500

    Updated Card.css, Home.css, and Home.jsx. I added 3 copies of the plant just to see grid visually.

--- plant-ish/src/components/Homepage/Home.jsx
+++ plant-ish/src/components/Homepage/Home.jsx
@@ -3,38 +3,68 @@ import { Link } from 'react-router-dom';
 import './Home.css';
 
 function Home({ itemList }) {
-    return (
-        <>
-          
-       <div className='header'>
-            Browse indoor plants!
-        </div>
-        <div className='body'>
-            Browse a wide variety of indoor house plants. Login for a personalized experience, bringing member exclusive content and capabilities.
-            </div>
- 
-            <div className='three-col'>
-                
+  return (
+    <>
+      <div className='header'>
+        Browse indoor plants!
+      </div>
+      <div className='body'>
+        Browse a wide variety of indoor house plants. Login for a personalized experience, bringing member exclusive content and capabilities.
+      </div>
 
-      {itemList.map((plant, index) => (
+      <div className='three-col'>
+        {itemList.map((plant, index) => (
           <div key={index} className='card'>
             <div className='container'>
+              <img src={plant.img} alt={plant.name} />
               <h3><b>{plant.name}</b></h3>
               <p>Maintenance Level: {plant.maintenanceLvl}</p>
               <p>Price: {plant.price}</p>
               <p>Rating: {plant.rating}</p>
+            </div>
+          </div>
+        ))}
+        {itemList.map((plant, index) => (
+          <div key={index} className='card'>
+            <div className='container'>
               <img src={plant.img} alt={plant.name} />
+              <h3><b>{plant.name}</b></h3>
+              <p>Maintenance Level: {plant.maintenanceLvl}</p>
+              <p>Price: {plant.price}</p>
+              <p>Rating: {plant.rating}</p>
+            </div>
+          </div>
+        ))}
+        {itemList.map((plant, index) => (
+          <div key={index} className='card'>
+            <div className='container'>
+              <img src={plant.img} alt={plant.name} />
+              <h3><b>{plant.name}</b></h3>
+              <p>Maintenance Level: {plant.maintenanceLvl}</p>
+              <p>Price: {plant.price}</p>
+              <p>Rating: {plant.rating}</p>
+            </div>
+          </div>
+        ))}
+        {itemList.map((plant, index) => (
+          <div key={index} className='card'>
+            <div className='container'>
+              <img src={plant.img} alt={plant.name} />
+              <h3><b>{plant.name}</b></h3>
+              <p>Maintenance Level: {plant.maintenanceLvl}</p>
+              <p>Price: {plant.price}</p>
+              <p>Rating: {plant.rating}</p>
             </div>
           </div>
         ))}
       </div>
-            
-              
-     <Link to="/add">
+
+
+      <Link to="/add">
         <button className="add-button">Add Plant</button>
       </Link>
-        </>
-    );
+    </>
+  );
 }
 
 export default Home;
